b8692635ad373846d1257e4e757132b85e5b3030
Arseny Fedorchenko <Arantola@mail.ru>
2022-11-07 09:56:01 +0300

    feat: add solution to 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const { mkdir, readdir, copyFile } = require('fs');
+const { resolve } = require('path');
+const { stdout } = process;
 
+const readFrom = resolve(__dirname, 'files'),
+      writeTo  = readFrom + '-copy';
+
+function copyDir(readFrom, writeTo) {
+  mkdir(writeTo, { recursive: true }, err => {
+    if (err) throw err;
+  });
+
+  readdir(readFrom, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+
+    for(let file of files) {
+      if (file.isFile()) {
+        copyFile( resolve(readFrom, file.name),
+                  resolve(writeTo, file.name), err => {
+          if (err) throw err;
+        })
+      } else {
+        copyDir(resolve(readFrom, file.name),
+                resolve(writeTo, file.name));
+      }
+    }
+    stdout.write('Directory copied');
+  });
+}
+
+copyDir(readFrom, writeTo);
